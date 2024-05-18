af78585558fb465da6622485833cb02a968eb59f
William Chong <me@williamchong.cloud>
2024-05-19 02:37:25 +0800

    [ingest] add copy uploaded file to local dir

--- ingest/main.go
+++ ingest/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"ingest-v2/utils"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
+
+var outputDirectory = os.Getenv("FILE_OUTPUT_PATH")
+
+func copyOutputToFile(src io.Reader, originalFileName string, cid string) error {
+	ext := filepath.Ext(originalFileName)
+	path := filepath.Join(outputDirectory, cid)
+	if ext != "" {
+		path += ext
+	}
+	fd, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(fd, src)
+	return err
+}
 
 func main() {
 	r := gin.Default()
@@ -56,6 +75,8 @@ func main() {
 			})
 			return
 		}
+
+		originalFileName := files[0].Filename
 		file, err := files[0].Open()
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -64,7 +85,23 @@ func main() {
 			return
 		}
 		defer file.Close()
-		cid := utils.Cid(file)
+
+		var buf bytes.Buffer
+		teeFile := io.TeeReader(file, &buf)
+		cid := utils.Cid(teeFile)
+		if cid == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Failed to generate CID for the file.",
+			})
+			return
+		}
+		err = copyOutputToFile(&buf, originalFileName, cid)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 
 		metadatas := form.File["metadata"]
 		if len(metadatas) != 1 {
