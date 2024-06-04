05cf4ccfa537244a1814d8821e070e493d9cf047
makeworld <makeworld@protonmail.com>
2024-06-04 13:52:06 -0400

    change encryption key design: keys are named after file+attr

--- export-proof/export_proof.go
+++ export-proof/export_proof.go
@@ -11,11 +11,11 @@ import (
 )
 
 var (
-	cid     string
-	attr    string
-	format  string
-	output  string
-	keyName string
+	cid         string
+	attr        string
+	format      string
+	output      string
+	isEncrypted bool
 )
 
 func Run(args []string) error {
@@ -24,7 +24,7 @@ func Run(args []string) error {
 	fs.StringVar(&attr, "attr", "", "attribute")
 	fs.StringVar(&format, "format", "cbor", "proof format (cbor,vc)")
 	fs.StringVar(&output, "o", "", "output path")
-	fs.StringVar(&keyName, "key", "", "name of metadata encryption key (only needed for encrypted attributes)")
+	fs.BoolVar(&isEncrypted, "encrypted", false, "attribute is encrypted")
 
 	err := fs.Parse(args)
 	if err != nil {
@@ -50,9 +50,9 @@ func Run(args []string) error {
 
 	// Get key
 	var key []byte
-	if keyName != "" {
+	if isEncrypted {
 		var err error
-		key, err = os.ReadFile(filepath.Join(conf.Dirs.MetadataEncKeys, keyName))
+		key, err = os.ReadFile(filepath.Join(conf.Dirs.EncKeys, fmt.Sprintf("%s_%s.key", cid, attr)))
 		if err != nil {
 			return fmt.Errorf("error reading key: %w", err)
 		}
