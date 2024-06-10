4de28eedd61d6d70d5bf6dde29c836e7fd48ee4f
makeworld <makeworld@protonmail.com>
2024-06-10 10:32:41 -0400

    fix attribute encryption bug
    Arrays are never considered empty because they are full of zeros by
    default. So all attributes were getting encrypted (with a key of zeros)
    when set. Switching to a slice fixes this.
    
    https://github.com/golang/go/issues/29310

--- aa/aa.go
+++ aa/aa.go
@@ -59,10 +59,10 @@ type AttEntry struct {
 // Attributes for uploading.
 // See https://github.com/starlinglab/authenticated-attributes/blob/main/docs/http.md#post-ccid
 type PostKV struct {
-	Key    string   `cbor:"key"`
-	Value  any      `cbor:"value"`
-	Type   string   `cbor:"type,omitempty"`
-	EncKey [32]byte `cbor:"encKey,omitempty"`
+	Key    string `cbor:"key"`
+	Value  any    `cbor:"value"`
+	Type   string `cbor:"type,omitempty"`
+	EncKey []byte `cbor:"encKey,omitempty"` // 32 bytes
 }
 
 var (
